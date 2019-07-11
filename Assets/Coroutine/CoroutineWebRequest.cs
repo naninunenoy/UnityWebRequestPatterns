3c3d59685fc9fd8ca45310a95c7b49a11402e3db
nakano yosuke <naninuneno0411@gmail.com>
2019-07-11 22:12:42 +0900

    コルーチン

---
+++ Assets/Coroutine/CoroutineWebRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+namespace WebRequest {
+    public class CoroutineWebRequest : MonoBehaviour {
+        IEnumerator Start() {
+            var req = UnityWebRequest.Get("https://unity.com/");
+            yield return req.SendWebRequest();
+            Debug.Log(req.downloadHandler.text);
+        }
+    }
+}
