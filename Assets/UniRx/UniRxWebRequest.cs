2e41763c29ba97382832eeb986685a4343f1bde6
nakano yosuke <naninuneno0411@gmail.com>
2019-07-11 23:21:47 +0900

    UniRxWebRequest

---
+++ Assets/UniRx/UniRxWebRequest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UniRx;
+using UniRx.Async;
+
+namespace WebRequest.Observable {
+    public class UniRxWebRequest : MonoBehaviour {
+        async void Start() {
+            var req = UnityWebRequest.Get("https://unity.com/");
+            await req.SendWebRequest().ToUniTask();
+            Debug.Log(req.downloadHandler.text);
+        }
+
+        // 旧型式
+        //void Start() {
+        //    ObservableWWW
+        //        .Get("https://unity.com/")
+        //        .Subscribe(x => {
+        //            Debug.Log(x);
+        //        })
+        //        .AddTo(this);
+        //}
+    }
+}
